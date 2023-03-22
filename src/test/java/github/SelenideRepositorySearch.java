efd5d24e21b9929f1bad34daed18dfe4785bf518
Evgenii <sergeevjane@mail.ru>
2023-03-22 23:06:40 +0300

    added BestContributorToSelenide

--- src/test/java/github/SelenideRepositorySearch.java
+++ src/test/java/github/SelenideRepositorySearch.java
@@ -1,6 +1,7 @@
 package github;
 
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -11,7 +12,7 @@ public class SelenideRepositorySearch {
 
     @Test
     void shouldFindSelenideRepositoryAtTheTop() {
-
+        Configuration.browser = "firefox";
         // открыть главную страницу
         open("https://github.com/");
         // ввести в поле поиска selenide и нажать enter
@@ -22,11 +23,12 @@ public class SelenideRepositorySearch {
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
 
 
-        // ARRANGE
-        // ACT
-        // ACT
-        // (ASSERT)
-        // ACT
-        // ASSERT
+       // Относится ко всем видам теста, состоит из 3 вещей
+        // ARRANGE подготовка
+        // ACT     действие
+        // ASSERT  проверка
+
+        // .classname, #id1, [atribute] example a[href=abc].red - 3 условия
+
     }
 }
